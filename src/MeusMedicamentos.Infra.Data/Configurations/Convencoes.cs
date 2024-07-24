b02870e9b360e6f024f2692ee49bd8df436d0168
Danilo Silva <danilo.silva@msn.com>
2024-07-24 17:12:09 -0300

    chore: Update Convencoes.cs to apply naming conventions for tables, primary keys, foreign keys, and indexes

--- src/MeusMedicamentos.Infra.Data/Configurations/Convencoes.cs
+++ src/MeusMedicamentos.Infra.Data/Configurations/Convencoes.cs
@@ -1,67 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace MeusMedicamentos.Infra.Data.Configurations
+namespace MeusMedicamentos.Infra.Data.Configurations;
+
+public static class Convencoes
 {
-    public static class Convencoes
+    public static void AplicarConvencoes(ModelBuilder modelBuilder)
     {
-        public static void AplicarConvencoes(ModelBuilder modelBuilder)
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
-            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            // Configurar o nome da tabela para TB_NOME_DA_CLASSE
+            modelBuilder.Entity(entityType.ClrType).ToTable($"TB_{entityType.ClrType.Name.ToUpper()}");
+
+            // Configurar a chave primária para PK_NOME_DA_TABELA
+            var chavePrimaria = entityType.FindPrimaryKey();
+            if (chavePrimaria != null)
             {
-                // Configurar o nome da tabela para TB_NOME_DA_CLASSE
-                modelBuilder.Entity(entityType.ClrType).ToTable($"TB_{entityType.ClrType.Name.ToUpper()}", t =>
-                {
-                    // Configurar os checks para CK_NOME_DO_CHECK
-                    var checkConstraints = entityType.GetCheckConstraints();
-                    if (checkConstraints != null)
-                    {
-                        foreach (var constraint in checkConstraints)
-                        {
-                            var constraintName = constraint.Name?.ToUpper();
-                            var constraintSql = constraint.Sql;
-                            if (constraintName != null && constraintSql != null)
-                            {
-                                t.HasCheckConstraint($"CK_{constraintName}", constraintSql);
-                            }
-                        }
-                    }
-                });
+                chavePrimaria.SetName($"PK_{entityType.GetTableName()}");
+            }
 
-                // Configurar a chave primária para PK_NOME_DA_PROPRIEDADE
-                var chavePrimaria = entityType.FindPrimaryKey();
-                if (chavePrimaria != null)
-                {
-                    foreach (var key in chavePrimaria.Properties)
-                    {
-                        key.SetColumnName($"PK_{key.Name.ToUpper()}");
-                    }
-                }
+            // Configurar as chaves estrangeiras para FK_NOME_DA_TABELA
+            foreach (var foreignKey in entityType.GetForeignKeys())
+            {
+                foreignKey.SetConstraintName($"FK_{entityType.GetTableName()}_{foreignKey.PrincipalEntityType.GetTableName()}");
+            }
 
-                // Configurar as chaves estrangeiras para FK_NOME_DA_PROPRIEDADE
-                var foreignKeys = entityType.GetForeignKeys();
-                if (foreignKeys != null)
-                {
-                    foreach (var chaveEstrangeira in foreignKeys)
-                    {
-                        foreach (var key in chaveEstrangeira.Properties)
-                        {
-                            key.SetColumnName($"FK_{key.Name.ToUpper()}");
-                        }
-                    }
-                }
-
-                // Configurar os índices para IX_NOME_DA_PROPRIEDADE
-                var indexes = entityType.GetIndexes();
-                if (indexes != null)
-                {
-                    foreach (var indice in indexes)
-                    {
-                        foreach (var property in indice.Properties)
-                        {
-                            property.SetColumnName($"IX_{property.Name.ToUpper()}");
-                        }
-                    }
-                }
+            // Configurar os índices para IX_NOME_DA_TABELA
+            foreach (var index in entityType.GetIndexes())
+            {
+                index.SetDatabaseName($"IX_{entityType.GetTableName()}_{string.Join("_", index.Properties.Select(p => p.Name))}");
             }
         }
     }
