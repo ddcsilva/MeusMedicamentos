6c01112458847d20bee3a9cfb060fd71621e5568
Danilo Silva <danilo.silva@msn.com>
2024-07-24 17:12:16 -0300

    chore: Update CategoriaMapping.cs to configure primary key, specific properties, common properties, and indexes

--- src/MeusMedicamentos.Infra.Data/Mappings/CategoriaMapping.cs
+++ src/MeusMedicamentos.Infra.Data/Mappings/CategoriaMapping.cs
@@ -8,6 +8,9 @@ public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
 {
     public void Configure(EntityTypeBuilder<Categoria> builder)
     {
+        // Chave primária
+        builder.HasKey(c => c.Id);
+
         builder.Property(c => c.Id)
             .HasColumnName("CTG_ID")
             .HasColumnOrder(1)
@@ -15,22 +18,53 @@ public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
             .IsRequired()
             .HasComment("Chave primária da categoria");
 
+        // Propriedades específicas
         builder.Property(c => c.Nome)
             .HasColumnName("CTG_NOME")
             .HasColumnOrder(2)
             .HasMaxLength(100)
             .IsRequired()
             .HasColumnType("varchar(100)")
-            .HasComment("Nome da categoria");
+            .HasComment("Nome da Categoria");
+
+        // Propriedades comuns
+        builder.Property(c => c.Status)
+            .HasColumnName("CTG_STATUS")
+            .HasColumnOrder(3)
+            .IsRequired()
+            .HasColumnType("int")
+            .HasComment("Status da Categoria");
 
         builder.Property(c => c.DataCriacao)
             .HasColumnName("CTG_DATA_CRIACAO")
-            .HasColumnOrder(3)
+            .HasColumnOrder(4)
             .IsRequired()
             .HasColumnType("datetime")
-            .HasComment("Data de criação da categoria");
+            .HasComment("Data da Criação da Categoria");
 
+        builder.Property(c => c.DataModificacao)
+            .HasColumnName("CTG_DATA_MODIFICACAO")
+            .HasColumnOrder(5)
+            .HasColumnType("datetime")
+            .HasComment("Data da Última Modificação da Categoria");
+
+        builder.Property(c => c.UsuarioCriacao)
+            .HasColumnName("CTG_USUARIO_CRIACAO")
+            .HasColumnOrder(6)
+            .HasMaxLength(50)
+            .HasColumnType("varchar(50)")
+            .HasComment("Usuário Responsável pela Criação da Categoria");
+
+        builder.Property(c => c.UsuarioModificacao)
+            .HasColumnName("CTG_USUARIO_MODIFICACAO")
+            .HasColumnOrder(7)
+            .HasMaxLength(50)
+            .HasColumnType("varchar(50)")
+            .HasComment("Usuário Responsável pela Modificação da Categoria");
+
+        // Índices
         builder.HasIndex(c => c.Nome)
+            .HasDatabaseName("IX_CATEGORIA_NOME")
             .IsUnique();
     }
 }
