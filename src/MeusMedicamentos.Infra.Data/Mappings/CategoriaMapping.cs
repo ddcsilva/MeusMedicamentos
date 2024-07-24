fb9d3211520584476fae33a2cef1ab82fdc55e68
Danilo Silva <danilo.silva@msn.com>
2024-07-24 16:28:56 -0300

    feat: Update project references and add Entity Framework Core packages

---
+++ src/MeusMedicamentos.Infra.Data/Mappings/CategoriaMapping.cs
@@ -0,0 +1,35 @@
+using MeusMedicamentos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeusMedicamentos.Infra.Data.Mappings;
+
+public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.Property(c => c.Id)
+            .HasColumnName("CTG_ID")
+            .HasColumnOrder(1)
+            .HasColumnType("uniqueidentifier")
+            .HasComment("Chave primária da categoria");
+
+        builder.Property(c => c.Nome)
+            .HasColumnName("CTG_NOME")
+            .HasColumnOrder(2)
+            .HasMaxLength(100)
+            .IsRequired()
+            .HasColumnType("varchar(100)")
+            .HasComment("Nome da categoria");
+
+        builder.Property(c => c.DataCriacao)
+            .HasColumnName("CTG_DATA_CRIACAO")
+            .HasColumnOrder(3)
+            .IsRequired()
+            .HasColumnType("datetime")
+            .HasComment("Data de criação da categoria");
+
+        builder.HasIndex(c => c.Nome)
+            .IsUnique();
+    }
+}
