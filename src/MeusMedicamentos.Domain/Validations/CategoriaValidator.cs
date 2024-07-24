1118afffd1b1d302d7e87a7e8ca2625d626ae301
Danilo Silva <danilo.silva@msn.com>
2024-07-24 13:51:16 -0300

    chore: Update MeusMedicamentos.Domain.csproj and add CategoriaValidator class

---
+++ src/MeusMedicamentos.Domain/Validations/CategoriaValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using MeusMedicamentos.Domain.Entities;
+
+namespace MeusMedicamentos.Domain.Validations;
+
+public class CategoriaValidator : AbstractValidator<Categoria>
+{
+    public CategoriaValidator()
+    {
+        RuleFor(c => c.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} deve ser fornecido")
+            .Length(2, 100).WithMessage("O campo {PropertyName} deve ter entre {MinLength} e {MaxLength} caracteres");
+    }
+}
