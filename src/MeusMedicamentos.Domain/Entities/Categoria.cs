7ace75c2691433daaa4e34a4f77b7c05a9897b31
Danilo Silva <danilo.silva@msn.com>
2024-07-24 13:54:54 -0300

    feat: Add constructor to Categoria entity class

--- src/MeusMedicamentos.Domain/Entities/Categoria.cs
+++ src/MeusMedicamentos.Domain/Entities/Categoria.cs
@@ -2,5 +2,10 @@ namespace MeusMedicamentos.Domain.Entities;
 
 public class Categoria : Entity
 {
+    public Categoria(string nome)
+    {
+        Nome = nome;
+    }
+
     public string Nome { get; set; } = string.Empty;
 }
