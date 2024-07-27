e375ab8444b7c76dbfceb1debab060bd3fae8504
Danilo Silva <danilo.silva@msn.com>
2024-07-27 10:48:25 -0300

    Atualizar controlador de autenticação para importar DTOs e adicionar endpoint de criação de usuário

--- src/MeusMedicamentos.Web/Controllers/HomeController.cs
+++ src/MeusMedicamentos.Web/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-using MeusMedicamentos.Web.Models;
 using System.Diagnostics;
 
 namespace MeusMedicamentos.Web.Controllers
@@ -8,27 +7,9 @@ namespace MeusMedicamentos.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
