5c837f0929f7ac17ac70b16627bd41c04eed4f91
Danilo Silva <danilo.silva@msn.com>
2024-07-27 22:59:16 -0300

    Atualizar controladores para remover mensagens de erro desnecessárias

--- src/MeusMedicamentos.Web/Controllers/HomeController.cs
+++ src/MeusMedicamentos.Web/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using MeusMedicamentos.Domain.Notifications;
 
 namespace MeusMedicamentos.Web.Controllers;
 
 [Authorize]
-public class HomeController : Controller
+public class HomeController : MainController
 {
+    public HomeController(INotificadorErros notificadorErros)
+        : base(notificadorErros)
+    {
+    }
+
     public IActionResult Index()
     {
         return View();
