6d073b73a676910d911dba80a3a126431b2b2219
Danilo Silva <danilo.silva@msn.com>
2024-07-27 22:40:04 -0300

    feat: Atualizar controlador principal para herdar de ControllerBase e adicionar atributo ApiController

--- src/MeusMedicamentos.API/Controllers/CategoriasController.cs
+++ src/MeusMedicamentos.API/Controllers/CategoriasController.cs
@@ -5,68 +5,67 @@ using MeusMedicamentos.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MeusMedicamentos.API.Controllers
+namespace MeusMedicamentos.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+public class CategoriasController : MainController
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    public class CategoriasController : MainController
+    private readonly ICategoriaService _categoriaService;
+
+    public CategoriasController(ICategoriaService categoriaService, INotificadorErros notificadorErros)
+        : base(notificadorErros)
     {
-        private readonly ICategoriaService _categoriaService;
+        _categoriaService = categoriaService;
+    }
 
-        public CategoriasController(ICategoriaService categoriaService, INotificadorErros notificadorErros)
-            : base(notificadorErros)
+    [HttpGet]
+    public async Task<IActionResult> ObterTodos()
+    {
+        var response = await _categoriaService.ObterTodosAsync();
+        return CustomResponse(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> ObterPorId(Guid id)
+    {
+        var response = await _categoriaService.ObterPorIdAsync(id);
+        return CustomResponse(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Adicionar([FromBody] CriarCategoriaDTO categoriaDTO)
+    {
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+        var response = await _categoriaService.AdicionarAsync(categoriaDTO);
+        return CustomResponse(response);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Atualizar(Guid id, [FromBody] EditarCategoriaDTO categoriaDTO)
+    {
+        if (id != categoriaDTO.Id)
         {
-            _categoriaService = categoriaService;
+            NotificarErro("Erro ao atualizar a categoria: Id da requisição difere do Id do objeto");
+            return CustomResponse(new ApiResponse<string>("Erro ao atualizar a categoria: Id da requisição difere do Id do objeto", 400));
         }
 
-        [HttpGet]
-        public async Task<IActionResult> ObterTodos()
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+        var response = await _categoriaService.AtualizarAsync(categoriaDTO);
+        return CustomResponse(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remover(Guid id)
+    {
+        var response = await _categoriaService.RemoverAsync(id);
+        if (!response.Success)
         {
-            var response = await _categoriaService.ObterTodosAsync();
             return CustomResponse(response);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> ObterPorId(Guid id)
-        {
-            var response = await _categoriaService.ObterPorIdAsync(id);
-            return CustomResponse(response);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Adicionar([FromBody] CriarCategoriaDTO categoriaDTO)
-        {
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
-
-            var response = await _categoriaService.AdicionarAsync(categoriaDTO);
-            return CustomResponse(response);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Atualizar(Guid id, [FromBody] EditarCategoriaDTO categoriaDTO)
-        {
-            if (id != categoriaDTO.Id)
-            {
-                NotificarErro("Erro ao atualizar a categoria: Id da requisição difere do Id do objeto");
-                return CustomResponse(new ApiResponse<string>("Erro ao atualizar a categoria: Id da requisição difere do Id do objeto", 400));
-            }
-
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
-
-            var response = await _categoriaService.AtualizarAsync(categoriaDTO);
-            return CustomResponse(response);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Remover(Guid id)
-        {
-            var response = await _categoriaService.RemoverAsync(id);
-            if (!response.Success)
-            {
-                return CustomResponse(response);
-            }
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
--- src/MeusMedicamentos.API/Controllers/UsuariosController.cs
+++ src/MeusMedicamentos.API/Controllers/UsuariosController.cs
@@ -1,4 +1,3 @@
-using MeusMedicamentos.Application.DTOs;
 using MeusMedicamentos.Application.DTOs.Usuario;
 using MeusMedicamentos.Application.Interfaces;
 using MeusMedicamentos.Domain.Notifications;
@@ -6,75 +5,74 @@ using MeusMedicamentos.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MeusMedicamentos.API.Controllers
+namespace MeusMedicamentos.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+public class UsuariosController : MainController
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    public class UsuariosController : MainController
+    private readonly IUsuarioService _usuarioService;
+
+    public UsuariosController(IUsuarioService usuarioService, INotificadorErros notificadorErros)
+        : base(notificadorErros)
     {
-        private readonly IUsuarioService _usuarioService;
+        _usuarioService = usuarioService;
+    }
 
-        public UsuariosController(IUsuarioService usuarioService, INotificadorErros notificadorErros)
-            : base(notificadorErros)
+    [HttpGet]
+    public async Task<IActionResult> ObterTodos()
+    {
+        var response = await _usuarioService.ObterTodosUsuariosAsync();
+        return CustomResponse(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> ObterPorId(Guid id)
+    {
+        var response = await _usuarioService.ObterPorIdAsync(id);
+        return CustomResponse(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Adicionar([FromBody] CriarUsuarioDTO usuarioDTO)
+    {
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+        var response = await _usuarioService.CriarUsuarioAsync(usuarioDTO.UserName, usuarioDTO.Nome, usuarioDTO.Email);
+        return CustomResponse(response);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Atualizar(Guid id, [FromBody] EditarUsuarioDTO usuarioDTO)
+    {
+        if (id != usuarioDTO.Id)
         {
-            _usuarioService = usuarioService;
+            NotificarErro("Erro ao atualizar o usuário: Id da requisição difere do Id do objeto");
+            return CustomResponse(new ApiResponse<string>("Erro ao atualizar o usuário: Id da requisição difere do Id do objeto", 400));
         }
 
-        [HttpGet]
-        public async Task<IActionResult> ObterTodos()
-        {
-            var response = await _usuarioService.ObterTodosUsuariosAsync();
-            return CustomResponse(response);
-        }
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> ObterPorId(Guid id)
-        {
-            var response = await _usuarioService.ObterPorIdAsync(id);
-            return CustomResponse(response);
-        }
+        var result = await _usuarioService.AtualizarUsuarioAsync(id, usuarioDTO.UserName, usuarioDTO.Nome, usuarioDTO.Email);
+        return CustomResponse(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Adicionar([FromBody] CriarUsuarioDTO usuarioDTO)
-        {
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remover(Guid id)
+    {
+        var result = await _usuarioService.RemoverUsuarioAsync(id);
+        return CustomResponse(result);
+    }
 
-            var response = await _usuarioService.CriarUsuarioAsync(usuarioDTO.UserName, usuarioDTO.Nome, usuarioDTO.Email);
-            return CustomResponse(response);
-        }
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+    {
+        var token = await _usuarioService.AutenticarAsync(loginDTO.UserName, loginDTO.Senha);
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Atualizar(Guid id, [FromBody] EditarUsuarioDTO usuarioDTO)
-        {
-            if (id != usuarioDTO.Id)
-            {
-                NotificarErro("Erro ao atualizar o usuário: Id da requisição difere do Id do objeto");
-                return CustomResponse(new ApiResponse<string>("Erro ao atualizar o usuário: Id da requisição difere do Id do objeto", 400));
-            }
+        if (token == null)
+            return Unauthorized();
 
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
-
-            var result = await _usuarioService.AtualizarUsuarioAsync(id, usuarioDTO.UserName, usuarioDTO.Nome, usuarioDTO.Email);
-            return CustomResponse(result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Remover(Guid id)
-        {
-            var result = await _usuarioService.RemoverUsuarioAsync(id);
-            return CustomResponse(result);
-        }
-
-        [AllowAnonymous]
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
-        {
-            var token = await _usuarioService.AutenticarAsync(loginDTO.UserName, loginDTO.Senha);
-
-            if (token == null)
-                return Unauthorized();
-
-            return Ok(new { Token = token });
-        }
+        return Ok(new { Token = token });
     }
 }
