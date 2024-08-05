b80db7d9c8a1f3cc91c43e700711772c621ef1c2
Danilo Silva <danilo.silva@msn.com>
2024-08-05 10:33:06 -0300

    feat: Adicionar serviço de injeção de dependência para o módulo de categorias

--- Backend/MeusMedicamentos/MeusMedicamentos.Api/Program.cs
+++ Backend/MeusMedicamentos/MeusMedicamentos.Api/Program.cs
@@ -1,21 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Adição de Serviços ao Container de Injeção de Dependência
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+// Configuração da Pipeline de Requisição HTTP
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
