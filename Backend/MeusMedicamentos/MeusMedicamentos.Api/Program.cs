dbd50f611a1e56a53af2c80bfe2fc4781621db2e
Danilo Silva <danilo.silva@msn.com>
2024-08-05 11:04:58 -0300

    feat: Adicionar configuração de CORS e integração com IIS

--- Backend/MeusMedicamentos/MeusMedicamentos.Api/Program.cs
+++ Backend/MeusMedicamentos/MeusMedicamentos.Api/Program.cs
@@ -1,12 +1,35 @@
+using MeusMedicamentos.Api.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Adição de Serviços ao Container de Injeção de Dependência
+builder.Services.ConfigurarCors();
+builder.Services.ConfigurarIntegracaoIIS();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
 // Configuração da Pipeline de Requisição HTTP
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors();
 
 app.UseAuthorization();
 
