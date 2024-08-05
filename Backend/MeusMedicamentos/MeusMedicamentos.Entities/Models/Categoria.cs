e0868799f9d6a381543798f05f14fcd0db8d8ffd
Danilo Silva <danilo.silva@msn.com>
2024-08-05 13:57:15 -0300

    feat: Adicionar validações de dados nas classes Categoria e Medicamento

--- Backend/MeusMedicamentos/MeusMedicamentos.Entities/Models/Categoria.cs
+++ Backend/MeusMedicamentos/MeusMedicamentos.Entities/Models/Categoria.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MeusMedicamentos.Entities.Models;
 
 public class Categoria
 {
+    [Column("CategoriaId")]
+    public Guid Id { get; set; }
     
+    [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+    [MaxLength(60, ErrorMessage = "O nome da categoria deve ter no máximo 60 caracteres.")]
+    public string? Nome { get; set; }
+    
+    public ICollection<Medicamento>? Medicamentos { get; set; }
 }
--- Backend/MeusMedicamentos/MeusMedicamentos.Entities/Models/Medicamento.cs
+++ Backend/MeusMedicamentos/MeusMedicamentos.Entities/Models/Medicamento.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MeusMedicamentos.Entities.Models;
 
 public class Medicamento
 {
+    [Column("MedicamentoId")]
+    public Guid Id { get; set; }
     
+    [Required(ErrorMessage = "O nome do medicamento é obrigatório.")]
+    [MaxLength(60, ErrorMessage = "O nome do medicamento deve ter no máximo 60 caracteres.")]
+    public string? Nome { get; set; }
+    
+    [ForeignKey(nameof(Categoria))]
+    public Guid CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
 }
