eac84af92ac87b2fef3f5c1ab9060df7688dc277
Danilo Silva <danilo.silva@msn.com>
2024-08-05 10:56:40 -0300

    Métodos de Extensão

---
+++ Backend/MeusMedicamentos/MeusMedicamentos.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+namespace MeusMedicamentos.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurarCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+           options.AddPolicy("CorsPolicy", builder =>
+           {
+               builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+           });
+        });
+    }
+
+    public static void ConfigurarIntegracaoIIS(this IServiceCollection services)
+    {
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+    }
+}
